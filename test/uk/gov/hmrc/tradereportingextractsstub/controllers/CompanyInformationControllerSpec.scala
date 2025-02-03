f577be1bb496240d7d5b5a1256b44e9e914f318f
GaneshMaddipoti <16971000+GaneshMaddipoti@users.noreply.github.com>
2025-02-03 13:40:54 +0000

    TRE-236: Added unit test.

--- test/uk/gov/hmrc/tradereportingextractsstub/controllers/CompanyInformationControllerSpec.scala
+++ test/uk/gov/hmrc/tradereportingextractsstub/controllers/CompanyInformationControllerSpec.scala
@@ -5,15 +5,17 @@ import org.scalatest.wordspec.AnyWordSpec
 import play.api.http.Status
 import play.api.test.Helpers._
 import play.api.test.{FakeRequest, Helpers}
+import uk.gov.hmrc.tradereportingextractsstub.services.CompanyInformationService
 
 class CompanyInformationControllerSpec extends AnyWordSpec with Matchers {
 
-  private val fakeRequest = FakeRequest("GET", "/")
-  private val controller = new CompanyInformationController(Helpers.stubControllerComponents())
+  private val fakeRequest = FakeRequest("GET", "/company-information")
+  private val companyInformationService = new CompanyInformationService
+  private val controller = new CompanyInformationController(companyInformationService, Helpers.stubControllerComponents())
 
-  "GET /" should {
+  "GET /company-information" should {
     "return 200" in {
-      val result = controller.hello()(fakeRequest)
+      val result = controller.companyInformation()(fakeRequest)
       status(result) shouldBe Status.OK
     }
   }
